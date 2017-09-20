5540cb8787f67a41010d1a0a6898f85f62337293
Brendan Duke <dukebw@mcmaster.ca>
2017-09-19 20:54:21 -0400

    Hello world changes and sum changes (still need to add check on host).

--- hello.cu
+++ hello.cu
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdio.h>
 
 __global__ void hello_from_gpu(void)
@@ -10,7 +11,17 @@ int main(void)
         printf("Hello world from CPU!\n");
         hello_from_gpu<<<1, 10>>>();
 
-        cudaError_t cudaerr = cudaDeviceSynchronize();
+        int32_t runtime_version;
+        cudaError_t cudaerr = cudaRuntimeGetVersion(&runtime_version);
+        assert(cudaerr == cudaSuccess);
+
+        int32_t driver_version;
+        cudaerr = cudaDriverGetVersion(&driver_version);
+        assert(cudaerr == cudaSuccess);
+
+        printf("Runtime: %d, Driver: %d\n", runtime_version, driver_version);
+
+        cudaerr = cudaDeviceSynchronize();
         if (cudaerr != cudaSuccess)
                 printf("Kernel launch failed with error %s\n",
                        cudaGetErrorString(cudaerr));
--- sum_arrays_on_device.cu
+++ sum_arrays_on_device.cu
@@ -5,40 +5,34 @@
 #include <time.h>
 #include <assert.h>
 
-__global__ void sum_arrays_on_device(float *c, float *a, float *b)
+__global__ void
+sum_arrays_on_device(float *c, float *a, float *b)
 {
         uint32_t i = threadIdx.x + blockIdx.x*blockDim.x;
         c[i] = a[i] + b[i];
-
-        /* printf("thread %d: %f + %f == %f\n", i, a[i], b[i], c[i]); */
-
-        /* printf("threadIdx: (%d, %d, %d), blockIdx: (%d, %d, %d), " */
-        /*        "blockDim: (%d, %d, %d), gridIdx: (%d, %d, %d)\n", */
-        /*        threadIdx.x, threadIdx.y, threadIdx.z, */
-        /*        blockIdx.x, blockIdx.y, blockIdx.z, */
-        /*        blockDim.x, blockDim.y, blockDim.z, */
-        /*        gridDim.x, gridDim.y, gridDim.z); */
 }
 
-void initialize_data(float *device_data, const uint32_t num_floats)
+void initialize_data(float *host_data,
+                     float *device_data,
+                     const uint32_t num_floats)
 {
         cudaError_t status;
         time_t t;
         uint32_t size_bytes = sizeof(float)*num_floats;
-        float *temp_data = (float *)malloc(size_bytes);
 
         srand((uint32_t)time(&t));
 
         for (uint32_t i = 0;
              i < num_floats;
              ++i) {
-                temp_data[i] = (float)(rand() & 0xFF)/10.0f;
+                host_data[i] = (float)(rand() & 0xFF)/10.0f;
         }
 
-        status = cudaMemcpy(device_data, temp_data, size_bytes, cudaMemcpyHostToDevice);
+        status = cudaMemcpy(device_data,
+                            host_data,
+                            size_bytes,
+                            cudaMemcpyHostToDevice);
         assert(status == cudaSuccess);
-
-        free(temp_data);
 }
 
 int main(void)
@@ -47,8 +41,8 @@ int main(void)
         float *a;
         float *b;
         float *c;
-        const uint32_t num_floats = 32;
-        const uint32_t size_bytes = sizeof(float)*num_floats;
+        constexpr uint32_t num_floats = 32;
+        constexpr uint32_t size_bytes = sizeof(float)*num_floats;
 
         status = cudaMalloc(&a, size_bytes);
         assert(status == cudaSuccess);
@@ -59,11 +53,13 @@ int main(void)
         status = cudaMalloc(&c, size_bytes);
         assert(status == cudaSuccess);
 
-        initialize_data(a, num_floats);
-        initialize_data(b, num_floats);
+        float a_host[num_floats];
+        float b_host[num_floats];
+        initialize_data(a_host, a, num_floats);
+        initialize_data(b_host, b, num_floats);
 
-        dim3 block{num_floats/4};
-        dim3 grid{(num_floats + (block.x - 1))/block.x};
+        dim3 block = num_floats/4;
+        dim3 grid = (num_floats + (block.x - 1))/block.x;
         sum_arrays_on_device<<<grid, block>>>(c, a, b);
 
         status = cudaDeviceSynchronize();
